4f4824681a61493c55dcbc782a276aa632aa4ef8
Luiz <fellipevantine12@gmail.com>
2024-04-02 21:22:55 -0300

    Operadores_condicionais

---
+++ 13_operadores_condicionais.php
@@ -0,0 +1,10 @@
+<?php 
+
+$number = 12;
+$numberTwo = 12;
+
+if ($number == $numberTwo && $number == 12) {
+        echo 'E igual!';
+
+        echo 'Outro algo!';
+}
---
+++ 14_operadores_condicionais.php
@@ -0,0 +1,22 @@
+<?php 
+
+$numberOne = 15; 
+$numberTwo = 16;
+
+if ($numberOne == $numberTwo) {
+        echo 'E igual!';
+} else {
+        echo 'E diferente';
+}
+
+echo '<br>';
+
+if ($numberOne < 20) {
+
+        echo 'E menor';
+
+} else {
+
+        echo 'E maior!';
+        
+}
