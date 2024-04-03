4adda0450c3390a5c8df281bb6bd6fcd199bf9a7
Luiz <fellipevantine12@gmail.com>
2024-04-02 21:22:25 -0300

    Concatenacao

---
+++ 12_concatenacao.php
@@ -0,0 +1,21 @@
+<?php 
+
+$var1 = 'Algo';
+$var2 = 'Outro';
+
+echo $var1 . ' ' . $var2 . 12;
+
+echo '<br>';
+
+$var3 = $var1 . ' ' . $var2;
+
+echo $var3;
+
+echo '<br>';
+
+$name = 'Luiz Vantine';
+$company = 'EspecializaTi';
+
+// echo 'O ' . $name .' e aluno da '. $company;
+
+echo "O $name e aluno da $company";
