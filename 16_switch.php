ba1bb4d72f67ebe78dc98843f8d06b368db8c4ab
Luiz <fellipevantine12@gmail.com>
2024-04-05 19:54:32 -0300

    switch&loops

---
+++ 16_switch.php
@@ -0,0 +1,31 @@
+<?php 
+
+$number = 10;
+
+switch ($number) {
+
+    case $number == 12;
+        echo 'Doze';
+
+    break;
+
+    case $number == 13;
+        echo 'Treze';
+
+    break;
+
+    case $number == 14;
+        echo 'Quatorze';
+
+    break;
+
+    case $number == 15;
+        echo 'Quinze';
+
+    break;
+
+    default:
+        echo 'Nao identificado...';
+
+        break;
+}
---
+++ 18_loops_repeticao.php
@@ -0,0 +1,8 @@
+<?php 
+
+$names = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h'];
+
+
+foreach ($names as $key => $name) {
+    echo "$name <br>"; 
+}
