d9c700dad023f90e2579af823b92673c3d5b5c1c
Luiz <fellipevantine12@gmail.com>
2024-04-12 21:05:42 -0300

    Operadores_aritimeticos

---
+++ 34_operadores_aritimeticos.php
@@ -0,0 +1,33 @@
+<?php 
+
+$a = 2;
+$b = 2;
+echo $a;
+
+echo '<br>';
+
+// $a + 1;
+$a++;
+echo $a;
+
+echo '<br>';
+
+// $a - 1;
+$a--;
+echo $a;
+
+echo '<br>';
+
+// $a - 1;
+-- $a;
+echo $a;
+
+    echo '<hr>';
+
+echo '<br>';
+
+echo $a + $b++;
+
+echo '<br>';
+
+echo $a + ++$b;
